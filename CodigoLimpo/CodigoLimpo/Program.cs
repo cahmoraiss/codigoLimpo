76cc4d2fd924baf60e025e825305a4a78765151b
cahmoraiss <38811070+cahmoraiss@users.noreply.github.com>
2018-04-28 09:59:40 -0300

    CodigoLimpo
    FirstCommit

---
+++ CodigoLimpo/CodigoLimpo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoLimpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal valor = 0;
+            string valorExtenso;
+
+            ConverterValorPorExtenso();
+        }
+
+        private static void ConverterValorPorExtenso()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
